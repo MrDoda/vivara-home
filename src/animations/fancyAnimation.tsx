ccd9e53b802f92a0ac54c3200a51a1d2a3f16c78
Rammfon <125909369+Rammfon@users.noreply.github.com>
2024-02-04 22:25:29 +0100

    Inicialní commit projektu

---
+++ src/animations/fancyAnimation.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+// Importuj příslušné knihovny pro animaci
+
+const YourAnimation = () => {
+  useEffect(() => {
+    // Zde bude logika pro inicializaci a spuštění animace
+  }, []);
+
+  return <div id="animation-container"></div>;
+};
+
+export default YourAnimation;
---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Head from "next/head";
+import ClientSection from "../components/ClientSection";
+import About from "../components/about";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Moje Statická Stránka</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col justify-center items-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-6">Fuck my life</h1>
+        <div id="ClientSection" className="w-full">
+          <ClientSection />
+        </div>
+        <div id="About" className="w-full">
+          <About />
+        </div>
+      </main>
+
+      {/* Footer */}
+    </div>
+  );
+}
