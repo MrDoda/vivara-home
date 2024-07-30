2c19472d5529022ec0351b360d5daa2e4dee6b71
Rammfon <125909369+Rammfon@users.noreply.github.com>
2024-07-30 20:35:43 +0200

    remove comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-import { Navbar } from "../components/Navbar";
+
 import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
 
